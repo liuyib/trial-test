0c35b8ff2e36f02c4152fd393374ef08809cdcbc
liuyib <1656081615@qq.com>
2019-06-04 10:38:51 +0800

    Change ci test

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
-class OpenSourceTest {
-  constructor() {
-    this.test = {};
+var fibonacci = function (n) {
+  if (typeof n !== 'number') {
+    throw new Error('n should be a Number');
+  }
+  if (n < 0) {
+    throw new Error('n should >= 0')
+  }
+  if (n > 10) {
+    throw new Error('n should <= 10');
+  }
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
   }
 
-  set(key, value) {
-    this.test[key] = value;
-  }
+  return fibonacci(n-1) + fibonacci(n-2);
+};
 
-  get(key) {
-    return this.test[key];
-  }
-
-  example() {
-    this.test = {
-      'arr': [0, 1, 2, 3, 4, 5, 6]
-    };
-  }
-
-  clear() {
-    this.test = {};
-  }
-}
-
-window.OpenSourceTest = OpenSourceTest;
+exports.fibonacci = fibonacci;
